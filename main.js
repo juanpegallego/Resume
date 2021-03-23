0c2fe027e0c6ec721b2f6f6e7d580a7aab344257
Juanchi <gallegojpe@gmail.com>
2021-03-22 21:10:43 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const img = document.getElementById('innerImg').addEventListener('click', start)
+language();
+
+function start(){
+        document.getElementById('imgContainer').className = 'imgContainer1';
+        
+        setTimeout(() => {
+        document.getElementById('titleContainer').className = 'title-container1';
+        
+       }, 2900);     
+
+        setTimeout(() => { 
+        document.getElementById('info').className = 'info-container1';
+        document.getElementById('exp').className = 'exp-container1';
+        document.getElementById('skills').className = 'skills-container1';
+        document.getElementById('edu').className = 'edu-container1';
+        document.getElementById('iconContainer').className = 'icon-container1';
+        
+    }, 1500);
+
+    setTimeout(() => {
+        
+        document.getElementById('iconContainer').style.display = 'flex';
+        document.getElementById('iconContainer').style.transition = 'all 4s';
+        document.getElementById('dataContainer').style.display = 'flex';
+        document.getElementById('dataContainer').style.transition = 'all 4s';
+        
+    }, 5200);
+    }
+
+    
+    function language(){
+        if(window.location.pathname == "/index.html"){
+            document.getElementById('spanishBtn').style.border = '2.5px solid rgb(255, 209, 5)';
+            document.getElementById('spanishBtn').style.borderRadius = '25px';
+            document.getElementById('spanishBtn').style.color ='rgb(255, 209, 5)';
+            
+        }
+        if ((window.location.pathname == "/indexen.html")){
+            document.getElementById('englishBtn').style.border = '2.5px solid rgb(255, 209, 5)';
+            document.getElementById('englishBtn').style.borderRadius = '25px';
+            document.getElementById('englishBtn').style.color ='rgb(255, 209, 5)';
+        }
+    }
+    
